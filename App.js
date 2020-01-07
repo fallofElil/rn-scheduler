ba1d2da0a3ecb8395ef27d9514fcca086f9acfaf
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-01-07 16:11:57 +0300

    changes App as class-component:

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { Container, Content, Grid, Row, Col } from 'native-base';
+import React, { Component } from 'react';
+import { Container, Content, List, ListItem } from 'native-base';
 import { Navbar } from './src/Navbar';
 import { AddTask } from './src/AddTask';
 
-export default function App() {
-  return (
-    <Container>
-      <Navbar title="Органайзер" />
-      <Content>
-        <AddTask />
-      </Content>
-    </Container>
-  );
+class App extends Component {
+  /*const [tasks, setTasks] = useState([])
+
+  const addTask = (title) => {
+    const newTask = {
+      id: Date.now().toString(),
+      title
+    }
+
+    setTasks(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title
+      }
+    ])
+  }*/
+
+  render() {
+    return (
+      <Container>
+        <Navbar title="Органайзер" />
+        <Content>
+          <AddTask />
+        </Content>
+      </Container>
+    );
+  }
 }
+
+export default App;
+/*
+<ListItem>
+          {tasks.map(task => {
+          return <List>{task.title}</List>
+          })}
+        </ListItem>
+ */
